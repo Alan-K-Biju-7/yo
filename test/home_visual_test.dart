f03b53fb2c7f579484c8861e0bf79488fcc2c6c2
Alan K Biju <alan.k.biju25@gmail.com>
2026-09-03 23:14:32 +0530

    Make release tests platform independent

--- test/home_visual_test.dart
+++ test/home_visual_test.dart
@@ -1,3 +1,6 @@
+@Tags(['golden'])
+library;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rset_student_app/core/session/session_store.dart';
